b47c0e3e87c39b65dd648b19aa1d581da48737d1
lliei0x <git@liei.cc>
2017-06-20 17:25:24 +0800

    function:添加首页弹窗

--- src/components/layout.vue
+++ src/components/layout.vue
@@ -5,11 +5,11 @@
         <img src="../assets/logo.png">
         <div class="head-nav">
           <ul class="nav-list">
-            <li>登录</li>
+            <li @click="lognClick">登录</li>
             <li class="nav-pile">|</li>
-            <li>注册</li>
+            <li @click="regClick">注册</li>
             <li class="nav-pile">|</li>
-            <li>关于</li>
+            <li @click="aboutClick">关于</li>
           </ul>
         </div>
       </div>
@@ -20,15 +20,50 @@
     <div class="app-foot">
       <p>© 2016 fishenal MIT</p>
     </div>
+
+    <my-dialog :isShow="isShowLognDialog" @on-close="closeDialog('isShowLognDialog')">
+      <logn-form></logn-form>
+    </my-dialog>
+
+    <my-dialog :isShow="isShowRegDialog" @on-close="closeDialog('isShowRegDialog')">
+      <reg-form></reg-form>
+    </my-dialog>
+
+    <my-dialog :isShow="isShowAboutDialog" @on-close="closeDialog('isShowAboutDialog')">
+      <p>本报告在调研数据的基础上，采用定性与定量相结合的方式深入分析了专车市场发展的驱动因素与阻碍因素、专车市场背后的产业格局、专车企业的竞争格局、用户对专车市场的依赖程度、专车对其他交通工具运力的补充效应等，通过这五个章节的研究反映专车市场的发展态势和面临的问题。报告力求客观、深入、准确地反映中国专车市场发展情况，为政府、企事业单位和社会各界提供决策依据。</p>
+    </my-dialog>
   </div>
 </template>
 
 <script>
+import Dialog from './dialog'
+import LognForm from './lognForm'
+import RegForm from './regForm'
 export default {
-  name: 'hello',
+  components: {
+    MyDialog: Dialog,
+    LognForm,
+    RegForm
+  },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      isShowLognDialog: false,
+      isShowRegDialog: false,
+      isShowAboutDialog: false
+    }
+  },
+  methods: {
+    lognClick () {
+      this.isShowLognDialog = true
+    },
+    regClick () {
+      this.isShowRegDialog = true
+    },
+    aboutClick () {
+      this.isShowAboutDialog = true
+    },
+    closeDialog (attr) {
+      this[attr] = false
     }
   }
 }
